72c05b50a8c885ebfe75268e9debd25080804c33
kartikpachori <114762821+kartikpachori@users.noreply.github.com>
2023-08-29 00:18:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long p = abs(n);
+        double ans = pow(x, p);
+        if(n < 0) ans = 1 / ans;
+        return ans;
+    }
+};
