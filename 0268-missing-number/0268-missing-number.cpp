d60c5befe5cfea5b02263b296ea855aec2c7223b
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-06-03 20:24:35 +0530

    Time: 12 ms (68.08%), Space: 20.4 MB (31.17%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {   
+        int n= nums.size();
+        int xor1=0;
+        int xor2=0;
+        for(int i=0;i<n;i++){
+            xor2 = xor2 ^ nums[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        // xor1 =xor1 ^ n;
+        return xor1 ^ xor2 ;
+    }
+};
