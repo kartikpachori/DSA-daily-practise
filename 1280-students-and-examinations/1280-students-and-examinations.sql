6fb30fbc17923afcd3be5c8b18cee3d7f1248f9d
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-07-02 11:06:36 +0530

    Time: 919 ms (68.03%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -14,8 +14,7 @@ ON
     s.student_id = e.student_id 
     AND sub.subject_name = e.subject_name
 GROUP BY 
-    s.student_id, 
-   
+    s.student_id,    
     sub.subject_name
 ORDER BY 
     s.student_id, 
