ce0a75f50274604092956ec41049f43d2cc7c508
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-07-02 19:44:29 +0530

    Time: 4 ms (79.71%), Space: 9.4 MB (11.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,56 @@
 class Solution {
+private:
+    bool valid(char ch) {
+        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    char toLowerCase(char ch) {
+    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
+        return ch;
+    else{
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+    bool checkPalindrome(string a) {
+    int s = 0;
+    int e = a.length()-1;
+
+    while(s<=e) {
+        if(a[s] != a[e])
+        {
+            return 0;       
+        }
+        else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
 public:
     bool isPalindrome(string s) {
-    string filteredStr = "";
-    
-    // Normalize the string: convert to lowercase and filter non-alphanumeric characters
-    for (char c : s) {
-        if (isalnum(c)) { // Check if the character is alphanumeric
-            filteredStr += tolower(c); // Convert to lowercase and add to filteredStr
+        
+        //faltu character hatado
+        string temp = "";
+        
+        for(int j=0; j<s.length(); j++) {   
+            if(valid(s[j])) {
+                temp.push_back(s[j]);
+            }
         }
-    }
-    
-    // Check if filteredStr is a palindrome
-    int n = filteredStr.size();
-    for (int i = 0; i < n / 2; ++i) {
-        if (filteredStr[i] != filteredStr[n - i - 1]) {
-            return false; // Not a palindrome
+        
+        //lowercase me kardo
+        for(int j=0; j<temp.length(); j++) { 
+            temp[j] = toLowerCase(temp[j]);
         }
+        
+        //check palindrome
+        return checkPalindrome(temp);
+        
     }
-    
-    return true; // It is a palindrome
-}
 };
