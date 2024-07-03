c189701a2d2ccf37345654d54c37851a8e1a0098
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-07-04 02:18:55 +0530

    Time: 891 ms (84.62%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,6 @@
 SELECT 
     r.contest_id,
-    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(DISTINCT user_id) FROM Users), 2) AS percentage
+    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT( user_id) FROM Users), 2) AS percentage
 FROM 
     Register r
 GROUP BY 
