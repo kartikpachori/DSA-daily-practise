302c7b5ddaa7b8814c1b6db041822edd1c8d6bfa
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-07-27 11:57:28 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (6.90%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,10 +1,26 @@
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         if (s.length() != goal.length()) {
+//             return false;
+//         }
+//         string doubled_s = s + s;
+//         return doubled_s.find(goal) != string::npos;
+//     }
+// };
 class Solution {
 public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) {
             return false;
         }
-        string doubled_s = s + s;
-        return doubled_s.find(goal) != string::npos;
+        int len = s.length();
+        for (int i = 0; i < len; ++i) {
+            if (s.substr(i) + s.substr(0, i) == goal) {
+                return true;
+            }
+        }
+        return false;
     }
 };
+
