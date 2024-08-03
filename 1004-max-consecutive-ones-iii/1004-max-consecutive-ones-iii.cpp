605ccc3eed1ad30b6e9deb491916b20002e9e095
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-08-03 15:56:43 +0530

    Time: 47 ms (26.15%), Space: 58.2 MB (7.31%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -7,7 +7,7 @@ public:
         while (r < n) {
             if (nums[r] == 0) zeros++;
             
-            while (zeros > k) {
+            if (zeros > k) {
                 if (nums[l] == 0) zeros--;
                 l++;
             }
