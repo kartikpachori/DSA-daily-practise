d7f6cd0d0712705126851ea7b671097011540e90
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-08-11 19:31:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *n = new Node(x);
+    
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
+}
+
+void printList(Node *head)
+{
+  // The purpose of below two loops is
+  // to test the created DLL
+  Node *temp=head;
+  if (temp != NULL) {
+    int ct1=0,ct2=0;
+  while (temp->next!=NULL)
+    temp=temp->next,ct1++;
+  while (temp->prev!=NULL)
+   temp = temp->prev,ct2++;
+   if(ct1!=ct2){
+       cout<<-1<<"\n";
+       return;
+   }
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL; 
+  Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+     if(root==NULL) root = head;
+  }     
+  head = root;
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+//Function to insert a new node at given position in doubly linked list.
+void addNode(Node *head, int pos, int data) {
+    Node* temp = head;
+    int count = 0;
+    
+    // Traverse the list to find the position where the new node should be inserted
+    while (count < pos) {
+        temp = temp->next;
+        count++;
+    }
+    
+    // Create the new node with the given data
+    Node* new_node = new Node(data);
+    
+    
+    // If inserting in the middle or at the end
+    if (temp->next != NULL) {
+        Node* front = temp->next;
+        front->prev = new_node;
+        new_node->next = front;
+    } else {
+        new_node->next = NULL;
+    }
+    
+    temp->next = new_node;
+    new_node->prev = temp;
+}
