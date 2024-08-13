91cef10968e550417013449d33c51d39de5f1aec
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-08-13 23:22:21 +0530

    Time: 984 ms (43.43%), Space: 0B (100.00%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,21 @@
+WITH RankedSalaries AS (
+    SELECT 
+        e.id, 
+        e.name, 
+        e.salary, 
+        e.departmentId,
+        d.name AS department,
+        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS salary_rank
+    FROM 
+        Employee e
+    JOIN 
+        Department d ON e.departmentId = d.id
+)
+SELECT 
+    department,
+    name AS Employee,
+    salary
+FROM 
+    RankedSalaries
+WHERE 
+    salary_rank <= 3;
