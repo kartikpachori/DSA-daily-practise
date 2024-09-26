db4d35ffba1fdb618e000b0da8c8c1d371cfdd14
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-09-26 19:57:18 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (69.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return climbstairshelp(n,dp);
-    }
- private: 
-    int climbstairshelp(int n , vector<int>&dp){
-        if(n<=2)
-          return n;
-        
-       if(dp[n]!=-1) return dp[n];
-        
-       dp[n] = climbstairshelp(n-1,dp)+ climbstairshelp(n-2,dp);
-          return dp[n];
+        if (n <= 1) return 1;  
+        int prev = 1;  
+        int prev2 = 1; 
+        for (int i = 2; i <= n; i++) {
+            int curr = prev + prev2;  
+            prev2 = prev;  
+            prev = curr;   
+        }
+        return prev;  
     }
 };
-
