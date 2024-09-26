c4ecc488f7efe2737baebbd7279e482bbeb43d49
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-09-26 19:38:43 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (38.20%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        if (n<=2){
-            return n;
-        }
         return climbstairshelp(n,dp);
     }
  private: 
