48a7861e2f1d861bf969f0f50ce6fcb2b3724601
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-10-06 15:59:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int n, int m, vector<vector<int>>& grid) {
+      vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+    for (int j1 = 0; j1 < m; j1++) {
+        for (int j2 = 0; j2 < m; j2++) {
+            if (j1 == j2)
+                dp[n - 1][j1][j2] = grid[n - 1][j1];
+            else
+                dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+        }
+    }
+
+
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                int maxi = INT_MIN;
+
+                
+                for (int di = -1; di <= 1; di++) {
+                    for (int dj = -1; dj <= 1; dj++) {
+                        int ans;
+
+                        if (j1 == j2)
+                            ans = grid[i][j1];
+                        else
+                            ans = grid[i][j1] + grid[i][j2];
+
+                
+                        if ((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m))
+                            ans += -1e9; 
+                        else
+                            ans += dp[i + 1][j1 + di][j2 + dj]; 
+                        maxi = max(ans, maxi);
+                    }
+                }
+                dp[i][j1][j2] = maxi; 
+            }
+        }
+    }
+
+    
+    return dp[0][0][m - 1];
+}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid;
+        for (int i = 0; i < n; i++) {
+            vector<int> temp;
+            for (int j = 0; j < m; j++) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            grid.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.solve(n, m, grid) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
