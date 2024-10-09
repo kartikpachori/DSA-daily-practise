cce275b3b90bd4db7657dc4f2fa55739924e796d
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-10-09 23:38:36 +0530

    Time: 3 ms (97.94%), Space: 20.4 MB (83.49%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root, long long minVal = LLONG_MIN, long long maxVal = LLONG_MAX) {
+        if (root == nullptr) {
+            return true;
+        }
+ 
+        if (root->val <= minVal || root->val >= maxVal) {
+            return false;
+        }
+   
+        return isValidBST(root->left, minVal, root->val) && isValidBST(root->right, root->val, maxVal);
+    }
+};
