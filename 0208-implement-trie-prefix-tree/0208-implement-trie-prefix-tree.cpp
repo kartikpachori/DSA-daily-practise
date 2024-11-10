a17608bffb1204b41c9f18b6601a7a73dcd266f6
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2024-11-10 12:52:55 +0530

    Time: 42 ms (22.22%), Space: 46.9 MB (86.26%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,57 @@
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool isEndOfWord; 
+    
+    TrieNode() : isEndOfWord(false) {}
+};class Trie {
+private:
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode(); 
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                node->children[c] = new TrieNode();
+            }
+
+            node = node->children[c];
+        }
+        node->isEndOfWord = true;
+    }
+    
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return node->isEndOfWord;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char c : prefix) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return true; 
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
