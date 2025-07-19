35a157a0d2fecf5c66e6db3e88d4bc480bfebf42
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2025-07-19 20:37:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,60 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
 public:
-    int maximumPoints(vector<vector<int>>& arr, int n) {
-        vector<int> prev(4, 0);
-        
-     
-        prev[0] = max(arr[0][1], arr[0][2]);
-        prev[1] = max(arr[0][0], arr[0][2]);
-        prev[2] = max(arr[0][0], arr[0][1]);
-      
-        prev[3] = max(arr[0][0], max(arr[0][1], arr[0][2]));
-        
-        for (int day = 1; day < n; day++) {  
-            vector<int> temp(4, 0);
-            for (int last = 0; last < 4; last++) {
-                temp[last] = 0;
-                for (int task = 0; task < 3; task++) {
-                    if (task != last) {
-                        temp[last] = max(temp[last], arr[day][task] + prev[task]);
-                    }
+    int solve(int day, int last, vector<vector<int>>& arr, vector<vector<int>>& dp) {
+        if (day == 0) {
+            int maxi = 0;
+            for (int task = 0; task < 3; task++) {
+                if (task != last) {
+                    maxi = max(maxi, arr[0][task]);
                 }
             }
-            prev = temp;   
+            return maxi;
         }
-        return prev[3];
+
+        if (dp[day][last] != -1) return dp[day][last];
+
+        int maxi = 0;
+        for (int task = 0; task < 3; task++) {
+            if (task != last) {
+                int points = arr[day][task] + solve(day - 1, task, arr, dp);
+                maxi = max(maxi, points);
+            }
+        }
+
+        return dp[day][last] = maxi;
+    }
+
+    int maximumPoints(vector<vector<int>>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+        return solve(n - 1, 3, arr, dp); // 3 means no previous task
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> arr;
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            arr.push_back(temp);
-        }
-
-        Solution obj;
-        cout << obj.maximumPoints(arr, n) << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
