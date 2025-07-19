2895683ca77a99cb26cf46ed781e3f53b99aa753
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2025-07-19 21:42:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,62 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-  private:
-     void dfs(int node,vector<vector<int>>& adj,int vis[],vector<int> &ls){
-         vis[node] =1;
-         ls.push_back(node);
-         
-         for(auto it: adj[node]){
-             if(!vis[it]){
-                 dfs(it,adj,vis,ls);
-             }
-         }
-     }
   public:
-    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
-       int V = adj.size();
-       int vis[V] = {0};
-       int start = 0;
-       
-       vector<int>ls;
-       dfs(start,adj,vis,ls);
-       return ls;
-       
+    void dfsHelper(int node, vector<vector<int>>& adj, vector<int>& visited, vector<int>& result) {
+        visited[node] = 1;
+        result.push_back(node);
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfsHelper(neighbor, adj, visited, result);
+            }
+        }
+    }
+
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> visited(V, 0);
+        vector<int> result;
+        
+        dfsHelper(0, adj, visited, result);
+        return result;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        vector<vector<int>> adj(
-            V); // Use vector of vectors instead of array of vectors.
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-
-        Solution obj;
-        vector<int> ans = obj.dfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
