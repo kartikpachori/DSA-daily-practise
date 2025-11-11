af100e3bb23691bad6ec910c05eb8175fe970a92
KARTIKEY PACHORI <114762821+kartikpachori@users.noreply.github.com>
2025-11-11 21:15:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,41 +1,46 @@
 class Solution {
   public:
-    void merge(vector<int> &arr,int low,int mid,int high){
-        int left = low;
-        int right = mid+1;
-        vector<int>temp;
-        
-        while(left<=mid && right<=high){
-            if(arr[left]<=arr[right]){
-                temp.push_back(arr[left]);
-                left++;
-            }
-            else{
-                temp.push_back(arr[right]);
-                right++;
-            }
-        }
-        while(left<=mid){
-            temp.push_back(arr[left]);
-            left++;
-        }
-        while(right<=high){
-             temp.push_back(arr[right]);
-            right++;
-        }
-        for(int i=low;i<=high;i++){
-            arr[i] =temp[i-low];
-        }
+  
+    void merge(vector<int>&arr,int l,int mid,int r){
+       vector<int>temp;
+       int left = l;
+       int right = mid+1;
+       
+       while(left<=mid && right<=r){
+           if(arr[left]<arr[right]){
+               temp.push_back(arr[left]);
+               left++;
+           }
+           else{
+               temp.push_back(arr[right]);
+               right++;
+           }
+       }
+       
+       while(left<=mid){
+           temp.push_back(arr[left]);
+           left++;
+       }
+       
+       while(right<=r){
+           temp.push_back(arr[right]);
+           right++;
+       }
+       
+       for(int i=l;i<=r;i++){
+           arr[i]= temp[i-l];
+       }
     }
-    void ms(vector<int> & arr,int low,int high){
-        if(low==high) return;
-        int mid = (low+high)/2;
-        ms(arr,low,mid);
-        ms(arr,mid+1,high);
-        merge(arr,low,mid,high);
+    
+    void ms(vector<int>&arr,int l,int r){
+        if(l>=r) return;
+        int mid = (l+r)/2;
+        ms(arr,l,mid);
+        ms(arr,mid+1,r);
+        merge(arr,l,mid,r);
     }
+    
     void mergeSort(vector<int>& arr, int l, int r) {
-       ms(arr,l,r);
-        
+        ms(arr,l,r);
     }
 };
